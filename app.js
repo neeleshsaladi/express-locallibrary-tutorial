207e112ddd4e89a7d0c1243830f95080342b1977
Neelesh <neelesh.saladi@gmail.com>
2020-05-01 13:28:59 -0500

    for heroku

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-const dotenv = require('dotenv');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); 
+const compression = require('compression');
+const dotenv = require('dotenv')
+const helmet = require('helmet');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog');
-var app = express();
-dotenv.config({path: '.env'});
-//Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://neeleshsa:webappsusing@cluster0-mnph0.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: True });
-var db = mongoose.connection;
+//setting up database
+const app = express();
+app.use(helmet());
+dotenv.config({ path: '.env' })
+const mongoose = require('mongoose');
+
+const dev_db_url = process.env.ATLAS_URI
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
@@ -25,11 +31,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
